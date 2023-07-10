a0607798cc76f868de5c0c02f4b64e581d40896d
Amanda <amandaoliveirapolari@gmail.com>
2023-07-10 17:10:31 -0300

    [chore] extra module removed

---
+++ pokedex/src/App.jsx
@@ -0,0 +1,12 @@
+import './AppStyle.js';
+import { ContainerApp } from './AppStyle.js';
+
+function App() {
+    return (
+        <ContainerApp>
+            <p>Página Inicial</p>
+        </ContainerApp>
+    );
+}
+
+export default App;
