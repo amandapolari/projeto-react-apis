d6218234e1a511ab96db08169f4dec80dd3613ac
Amanda <amandaoliveirapolari@gmail.com>
2023-07-14 16:23:17 -0300

    [style] page details style started

--- pokedex/src/Pages/PokemonDetailPage/PokemonDetailPage.jsx
+++ pokedex/src/Pages/PokemonDetailPage/PokemonDetailPage.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
-import { ContainerPokemonDetailPage } from './PokemonDetailPageStyle';
+import {
+    ContainerImgBack,
+    ContainerImgFront,
+    ContainerListCardDetails,
+    ContainerMoves,
+    ContainerPokemonDetailPage,
+    ContainerStats,
+    ContainerTypes,
+} from './PokemonDetailPageStyle';
 
 const PokemonDetailPage = () => {
     return (
         <ContainerPokemonDetailPage>
             <p>[PÁGINA DE DETALHES] POKEMON DETAIL PAGE</p>
+            <ContainerListCardDetails>
+                <ContainerImgFront>Imagem Frontal</ContainerImgFront>
+                <ContainerImgBack>Imagem Posterior</ContainerImgBack>
+                <ContainerStats>
+                    <p>HP:</p>
+                    <p>attack:</p>
+                    <p>defense:</p>
+                    <p>special-attack:</p>
+                    <p>special-defense:</p>
+                    <p>speed:</p>
+                </ContainerStats>
+                <ContainerTypes>
+                    <p>type 1:</p>
+                    <p>type 2:</p>
+                </ContainerTypes>
+                <ContainerMoves>
+                    <p>Moves</p>
+                    <p>move name 1:</p>
+                    <p>move name 2:</p>
+                    <p>move name 3:</p>
+                    <p>move name 4:</p>
+                </ContainerMoves>
+            </ContainerListCardDetails>
         </ContainerPokemonDetailPage>
     );
 };
